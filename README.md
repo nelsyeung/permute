c5ba1c28b271c1e0b8e347b7fe01be1ada4ceba7
Nelson Yeung <nelsyeung@icloud.com>
2015-02-10 19:36:15 +0000

    Added range protection

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Permute
-C++ function to run through all permutations of a system with length of a set _n_ and _k_ elements.
-Requires C++11 and [Base conversion](https://github.com/nelsyeung/base-conversion).
+C++ function to run through all permutations of a system with length of a set _n_ and _k_ elements, where _n_ and _k_ are ranged from 2 to 36.
+Requires C++11 and [Base Conversion](https://github.com/nelsyeung/base-conversion).
 
 # Usage
 ```cpp
 n::permute(int n, int k, void (*f)(const std::string &));
 n::permute(3, 2, own_func);
+returns 0 if ran successfully and -1 otherwise
 ```
 
 # Full example
+Note that the function passed into the permute function needs to take in a string as a parameter.
 ```cpp
 #include "permute.hpp"
 
--- permute.hpp
+++ permute.hpp
@@ -6,14 +6,22 @@
 #include "base_conversion.hpp"
 
 namespace n {
-    void permute(int n, int k, void (*f)(const std::string &)) {
+    int permute(int n, int k, void (*f)(const std::string &)) {
         int numPermutations = pow(k, n);
         std::string configuration;
+
+        // Make sure n and k are range from 2 to 36
+        if (n < 2 or k < 2 or n > 36 or k > 36) {
+            std::cerr << "Error: n and k need to range from 2 to 36." << std::endl;
+            return -1;
+        }
 
         for (int i = 0; i < numPermutations; i++) {
             configuration = convert_base(i, 10, k);
             (*f)(std::string(n - configuration.length(), '0').append(configuration));
         }
+
+        return 0;
     }
 } // namespace n
 
